3afbb417d1df12413ad2bf9f869de4d4f7f46a08
zhchchzh <815129937@qq.com>
2019-08-27 02:25:16 +0900

    add search page

---
+++ WebApplication1/SearchPage.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class SearchPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void GridView1_OnRowCommand(object sender, GridViewCommandEventArgs e)
+         {
+             if (e.CommandName == "ShowDetail")
+             {
+                 int index = Convert.ToInt32(e.CommandArgument);
+                 
+ 
+             }
+         }
+    }
+}
